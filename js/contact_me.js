27643a65b47b68be4dce6121bac85a270f384e3f
nigelb <nigel.blackmore@ericsson.com>
2023-08-07 09:32:04 +0200

    work

--- js/contact_me.js
+++ js/contact_me.js
@@ -23,7 +23,7 @@ $(function() {
 
       Email.send({
         SecureToken : "42a06028-ab10-485c-ad0c-e769f4b75078",
-        To : 'nigel@blackmore.de',
+        To : 'nigel.blackmore@redbeemedia.com',
         From : 'info@blackmore.de',
         Subject : "Kontakt: blackmore-coaching",
         Body : name + "(" + email + ")\n" + message
